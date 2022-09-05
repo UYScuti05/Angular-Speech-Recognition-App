3088d5974c6614b683c06cd3a1d9a7f8a785acec
UY Scuti <anshul.sharma05@outlook.com>
2022-09-05 23:17:37 +0530

    Speech recognition using web speech recognition API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VoiceRecognitionService } from './service/voice-recognition.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Speech-Recognition-App';
+  text:string="";
+  constructor(public service:VoiceRecognitionService){
+    this.service.init();
+  }
+  ngOnInit(){
+
+  }
+  startService(){
+    this.service.start()
+  }
+ 
+  stopService(){
+    this.service.stop()
+  }
 }
